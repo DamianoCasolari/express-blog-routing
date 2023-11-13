339fbcd6e740b95e9eb108ba6e67eede417b7741
DamianoCasolari <damianocasolari@gmail.com>
2023-11-13 19:46:54 +0100

    add show method

--- controller/postsController.js
+++ controller/postsController.js
@@ -7,18 +7,18 @@ function index(req, res) {
         html: () => {
             const htmlContent = []
             htmlContent.push("<ul style='list-style:none'>")
-            for (const post of posts){
+            for (const post of posts) {
                 htmlContent.push(`
                     <li>
                     <h3>${post.title}</h3>
                     <p>${post.content}</p>
-                    <img src="/imgs/posts/${post.image}" alt="Immagine del post : ${post.title }">
+                    <img src="/imgs/posts/${post.image}" alt="Immagine del post : ${post.title}">
                     </li>`)
 
             }
             htmlContent.push("</ul>")
             res.send(htmlContent.join(""))
-            
+
         },
         json: () => {
             res.type("json").send(posts)
@@ -26,6 +26,18 @@ function index(req, res) {
     })
 }
 
+function show(req, res) {
+    const slug = req.params.slug
+    const singlePost = posts.filter((post) => {
+       return post.slug == slug
+    })
+    if (singlePost.length == 0) {
+        res.type("json").send("Il post cercato non esiste")    
+    }
+    res.type("json").send(singlePost)
+}
+
 module.exports = {
-    index
+    index,
+    show
 }
--- routers/posts.js
+++ routers/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require("../controller/postsController")
 
 router.get("/", postsController.index)
+router.get("/:slug", postsController.show)
 
 module.exports = router
